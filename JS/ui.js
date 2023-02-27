6d7bf1d04fa95c9bd25251f5e671c77f554fffea
Sanja <sanja.pistac@gmail.com>
2023-02-27 20:12:03 +0100

    Chuck Norris Joke Generator completed.

--- JS/ui.js
+++ JS/ui.js
@@ -2,6 +2,6 @@ const randomJoke = document.getElementById("random-joke");
 
 export function renderJoke(joke) {
 
-    const jokeHTML = `<p class="h4 text-success fw-bolder" >${joke}</p>`;
+    const jokeHTML = `<p class="h4 text-success m-4 fw-bolder" >${joke}</p>`;
     randomJoke.innerHTML = jokeHTML;
 }
